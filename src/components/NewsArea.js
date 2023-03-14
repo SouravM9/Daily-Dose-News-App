777d2bfae851e08297beef0bf6f6741824e625d6
SouravM9 <majumdars598@gmail.com>
2023-03-14 19:40:01 +0530

    Fetching data from API

--- src/components/NewsArea.js
+++ src/components/NewsArea.js
@@ -1,28 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import NewItem from './NewItem'
+import key from '../key.js'
 
 function NewsArea(props) {
-    const [title, setTitle] = useState('Ram Charan, His Wife Upasana Did This Ritual Before The Oscars To Keep Them Connected to India');
-    const [newsUrl, setNewsUrl] = useState('https://www.news18.com/movies/ram-charan-his-wife-upasana-did-this-ritual-before-the-oscars-to-keep-them-connected-to-india-7289815.html');
-    const [description, setDescription] = useState('During an interview, RRR actor Ram Charan revealed that he and his wife Upasana set up a temple wherever they go so they feel connected to India.');
-    const [publishedDate, setPublishedDate] = useState('2023-03-14 09:43:28');
-    const [imageUrl, setImageUrl] = useState('https://images.news18.com/ibnlive/uploads/2023/03/ram-charan-upasana-1.jpg');
-    const [source, setSource] = useState('News18');
+
+    const defaultImageUrl = 'https://www.euractiv.com/wp-content/uploads/sites/2/2014/03/news-default.jpeg';
+
+    const [dataList, setDataList] = useState([]);
+    const getNews = () => {
+
+        const url = 'https://newsdata2.p.rapidapi.com/news?country=in&language=en';
+
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': key,  // Env variable is not working
+                'X-RapidAPI-Host': 'newsdata2.p.rapidapi.com'
+            }
+        };
+
+        fetch(url, options)
+            .then(res => res.json())
+            .then(json => {
+                setDataList(json['results'])
+            })
+            .catch(err => console.error('error:' + err));
+    }
+
+    useEffect(() => {
+        getNews();
+
+    }, [])
 
     return (
         <>
             <h1>{props.title} Dose</h1>
             <div className="container d-flex flex-row mb-3 flex-wrap">
+                {dataList && dataList.map(item => (
 
-                <NewItem title={title} description={description} newsUrl={newsUrl} publishedDate={publishedDate} imageUrl={imageUrl} source={source} />
-                <NewItem title={title} description={description} newsUrl={newsUrl} publishedDate={publishedDate} imageUrl={imageUrl} source={source} />
-                <NewItem title={title} description={description} newsUrl={newsUrl} publishedDate={publishedDate} imageUrl={imageUrl} source={source} />
-                <NewItem title={title} description={description} newsUrl={newsUrl} publishedDate={publishedDate} imageUrl={imageUrl} source={source} />
-                <NewItem title={title} description={description} newsUrl={newsUrl} publishedDate={publishedDate} imageUrl={imageUrl} source={source} />
-                <NewItem title={title} description={description} newsUrl={newsUrl} publishedDate={publishedDate} imageUrl={imageUrl} source={source} />
-                <NewItem title={title} description={description} newsUrl={newsUrl} publishedDate={publishedDate} imageUrl={imageUrl} source={source} />
-                <NewItem title={title} description={description} newsUrl={newsUrl} publishedDate={publishedDate} imageUrl={imageUrl} source={source} />
-                <NewItem title={title} description={description} newsUrl={newsUrl} publishedDate={publishedDate} imageUrl={imageUrl} source={source} />
+                    <NewItem title={item.title}
+                        description={item.description}
+                        newsUrl={item.link}
+                        publishedDate={item.pubDate}
+                        imageUrl={item.image_url ? item.image_url : defaultImageUrl}
+                        source={item.source_id}
+                        key={item.link + item.title}
+                    />
+                ))}
+
             </div>
         </>
     )
