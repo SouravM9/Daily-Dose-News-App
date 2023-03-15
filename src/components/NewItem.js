338ee4f8fef8400f79ed05c7649fef89ef426b29
SouravM9 <majumdars598@gmail.com>
2023-03-15 17:51:34 +0530

    Infinite scroll added

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -4,13 +4,13 @@ function NewItem(props) {
     return (
 
         <div className="card m-3" style={{ width: "18rem" }}>
-            <img src={props.imageUrl} className="card-img-top" alt="..." />
+            <img src={props.imageUrl} className="card-img-top" alt="..." style={{ width: '285px', height: '165px' }} />
             <div className="card-body">
                 <h5 className="card-title">{props.title.substring(0, 45)}...</h5>
                 <p className="card-text">{props.description.substring(0, 95)}...</p>
-                <p className="card-text" style={{color : 'grey'}}>{new Date(props.publishedDate).toUTCString()}</p>
+                <p className="card-text" style={{ color: 'grey' }}>{new Date(props.publishedDate).toUTCString()}</p>
             </div>
-            
+
             <div className="card-body">
                 <a href={props.newsUrl} className="btn btn-primary">Read More</a>
             </div>
--- src/components/NewsArea.js
+++ src/components/NewsArea.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import NewItem from './NewItem'
 import key from '../key.js'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function NewsArea(props) {
 
     const defaultImageUrl = 'https://www.euractiv.com/wp-content/uploads/sites/2/2014/03/news-default.jpeg';
 
     const [dataList, setDataList] = useState([]);
-    const getNews = () => {
+    const [nextPage, setNextPage] = useState('');
+    const [hasMore, setHasMore] = useState(false);
+
+
+    const getNews = async () => {
 
         const url = 'https://newsdata2.p.rapidapi.com/news?country=in&language=en';
 
@@ -22,10 +27,48 @@ function NewsArea(props) {
         fetch(url, options)
             .then(res => res.json())
             .then(json => {
-                setDataList(json['results'])
+
+                if (json['nextPage']) {
+                    setHasMore(true);
+                    setNextPage(json['nextPage']);
+                }
+                setDataList(json['results']);
             })
             .catch(err => console.error('error:' + err));
     }
+
+    const fetchMoreData = async () => {
+
+        if (nextPage !== '') {
+            const url = `https://newsdata2.p.rapidapi.com/news?country=in&language=en&page=${nextPage}`;
+
+            const options = {
+                method: 'GET',
+                headers: {
+                    'X-RapidAPI-Key': key,  // Env variable is not working
+                    'X-RapidAPI-Host': 'newsdata2.p.rapidapi.com'
+                }
+            };
+
+            fetch(url, options)
+                .then(res => res.json())
+                .then(json => {
+
+                    if (json['nextPage']) {
+
+                        setHasMore(true);
+                        setNextPage(json['nextPage']);
+                    }
+                    let oldNews = dataList;
+                    setDataList(oldNews.concat(json['results']));
+                })
+                .catch(err => console.error('error:' + err));
+        }
+        else {
+            setHasMore(false);
+        }
+
+    };
 
     useEffect(() => {
         getNews();
@@ -35,20 +78,29 @@ function NewsArea(props) {
     return (
         <>
             <h1>{props.title} Dose</h1>
-            <div className="container d-flex flex-row mb-3 flex-wrap">
-                {dataList && dataList.map(item => (
+            <InfiniteScroll
+                dataLength={dataList.length}
+                next={fetchMoreData}
+                hasMore={hasMore}
+                loader={<h4>Loading...</h4>}
+            >
 
-                    <NewItem title={item.title}
-                        description={item.description}
-                        newsUrl={item.link}
-                        publishedDate={item.pubDate}
-                        imageUrl={item.image_url ? item.image_url : defaultImageUrl}
-                        source={item.source_id}
-                        key={item.link + item.title}
-                    />
-                ))}
+                <div className="container d-flex flex-row mb-3 flex-wrap">
+                    {dataList && dataList.map(item => (
 
-            </div>
+                        <NewItem title={item.title}
+                            description={item.description}
+                            newsUrl={item.link}
+                            publishedDate={item.pubDate}
+                            imageUrl={item.image_url ? item.image_url : defaultImageUrl}
+                            source={item.source_id}
+                            key={item.link + item.title}
+                        />
+                    ))}
+
+                </div>
+            </InfiniteScroll>
+
         </>
     )
 }
