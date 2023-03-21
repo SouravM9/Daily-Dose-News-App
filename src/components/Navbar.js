9c048f0481d5efd35a3528ef0294ee86855be5b4
SouravM9 <majumdars598@gmail.com>
2023-03-21 11:38:35 +0530

    Navbar sticks to top

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <div>
 
-            <nav className="navbar navbar-expand-lg bg-dark">
+            <nav className="navbar navbar-expand-lg bg-dark fixed-top">
                 <div className="container-fluid">
                     <Link className="navbar-brand text-light" to="/">Daily Dose</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
